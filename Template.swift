7e13c558d3734952cea2af825fb8ce3d32b27b18
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-09-04 21:30:12 -0400

    Create Template.swift

---
+++ Template.swift
@@ -0,0 +1,9 @@
+//
+//  Template.swift
+//
+//  Created by Joseph Wondimagnehu
+//  Created on Year-Month-Day
+//  Version 1.0
+//  Copyright (c) Year Joseph Wondimagnehu. All rights reserved.
+//
+//  Explain what the program does.
